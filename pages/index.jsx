6f571f727b2bc3b3dc8a12c368a22587f6a6fd59
Memowski <121754293+Memowski@users.noreply.github.com>
2023-01-01 17:21:22 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -43,6 +43,7 @@ Herkese merhaba, ben Mehmet. İnternet üzerinden para kazanma , kişisel geliş
             <span style={{ zIndex: '-1' }} className="text-white/5 absolute bottom-3 left-7 text-xl sm:text-2xl md:text-4xl lg:text-3xl font-semibold">Community Manager</span>
           </div>
 
+
     </>
   )
 }
