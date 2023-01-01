43bf8bd81aeab82e5de36bf16d8f2da7a9eb8d24
Memowski <121754293+Memowski@users.noreply.github.com>
2023-01-01 16:22:50 +0300

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -38,7 +38,7 @@ function MyApp({ Component, pageProps }) {
   return (<>
     <Head>
         <title>MVDıgıtal</title>
-        <link rel="shortcut icon" href="https://media.discordapp.net/attachments/1051536908443983893/1059098529538257006/memowski_pp_mini.png?width=1020&height=447" type="image/x-icon" ></link>
+        <link rel="shortcut icon" href="https://cdn.discordapp.com/attachments/1051536908443983893/1059098929909743717/memowski_pp_mini.png" type="image/x-icon" ></link>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link href="https://pro.fontawesome.com/releases/v5.15.4/css/all.css" rel="stylesheet" />
 
@@ -56,7 +56,7 @@ function MyApp({ Component, pageProps }) {
         <div style={{ zIndex: 99999 }} className="fixed bg-black/75 w-full h-screen flex justify-center items-center pointer-events-none">
             <div className="flex items-center gap-x-6 animate-pulse">
                 <div className="text-center">
-                    <p className="text-6xl mb-5 font-semibold">Umut Bayraktar</p>
+                    <p className="text-6xl mb-5 font-semibold">MVDigital</p>
                     <p className="uppercase text-xl font-semibold text-white"><i className="fal fa-spinner-third fa-spin" /></p>
                 </div>
             </div>
@@ -71,9 +71,9 @@ function MyApp({ Component, pageProps }) {
         <div className="max-w-screen-lg p-5 w-full md:w-10/12 lg:w-8/12 mx-auto transition-all duration-300">
           <div className="md:flex w-full items-center justify-between">
             <div>
-              <p>❤️ Umut Bayraktar</p></div>
+              <p>😏 MVDigital</p></div>
             <div className="mt-2 md:mt-0 flex items-center">
-              <a href="https://discord.gg/58e5H4try3" target="_blank" rel="noreferrer" className="w-full md:w-auto bg-neutral-700/5 hover:bg-neutral-700/20 px-4 py-2 rounded-md transition-all duration-200">
+              <a href="https://discord.com/invite/egHtAmapnp" target="_blank" rel="noreferrer" className="w-full md:w-auto bg-neutral-700/5 hover:bg-neutral-700/20 px-4 py-2 rounded-md transition-all duration-200">
                 <i className="fab fa-discord mr-2" />Join My Discord
               </a>
             </div>
