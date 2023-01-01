a38774d87908e5473eb03caa116f469011f0bb44
Memowski <121754293+Memowski@users.noreply.github.com>
2023-01-01 16:24:36 +0300

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -74,7 +74,7 @@ function MyApp({ Component, pageProps }) {
               <p>😏 MVDigital</p></div>
             <div className="mt-2 md:mt-0 flex items-center">
               <a href="https://discord.com/invite/egHtAmapnp" target="_blank" rel="noreferrer" className="w-full md:w-auto bg-neutral-700/5 hover:bg-neutral-700/20 px-4 py-2 rounded-md transition-all duration-200">
-                <i className="fab fa-discord mr-2" />Join My Discord
+                <i className="fab fa-discord mr-2" />Discorduma katılın!!
               </a>
             </div>
 
